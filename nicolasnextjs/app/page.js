ba9bc286cef088365edb40f10f349079cec0409d
vinith <vinithg407@gmail.com>
2026-02-17 21:58:26 -0500

    fix: remove duplicate AboutSection component from Page

--- nicolasnextjs/app/page.js
+++ nicolasnextjs/app/page.js
@@ -14,7 +14,6 @@ export default async function Page() {
       <HeroSection />
       <ServiceSections trimmingImages={trimmingImages} />
       <AboutSection />
-      <AboutSection />
       <MobileButton />
     </main>
   );
