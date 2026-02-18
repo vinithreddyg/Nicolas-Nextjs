a38460f1df49cd0cc4553f0899e899ea2e239554
vinith <vinithg407@gmail.com>
2026-02-17 19:02:14 -0500

    upgrade app to next js

--- nicolasnextjs/app/page.js
+++ nicolasnextjs/app/page.js
@@ -3,7 +3,7 @@ import ServiceSections from '../components/ServiceSections';
 import DisplayWorkImages from '../components/DisplayWorkImages';
 import AboutSection from '../components/AboutSection';
 import MobileButton from '../components/MobileButton';
-import getTrimmingImageUrls from '../lib/getTrimmingImageUrls';
+import getTrimmingImageUrls from '../public/lib/getTrimmingImageUrls';
 
 export default async function Page() {
   const trimmingImages = await getTrimmingImageUrls();
